d98a51858b9c912c097fc7d183086f77fad57d4b
rm7740 <adavid8a@hotmail.com>
2016-02-08 18:51:42 -0500

    Add a Tile array with instances of each Tile subclass

--- src/Tile.java
+++ src/Tile.java
@@ -1,15 +1,24 @@
+import org.omg.CORBA.PUBLIC_MEMBER;
+
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class Tile {
 
-    public static final int TILE_WIDTH = 64, TILE_HEIGHT = 64;
+    public static Tile[] tiles = new Tile[256];
+    public static Tile grassTile = new GrassTile(0);
+    public static Tile dirtTile = new DirtTile(1);
+    public static Tile rockTile = new RockTile(2);
+
+    public static final int TILE_WIDTH = 32, TILE_HEIGHT = 32;
     protected BufferedImage texture;
     protected final int ID;
 
     public Tile(BufferedImage texture, int id) {
         this.texture = texture;
         this.ID = id;
+
+        tiles[id] = this;
     }
 
     public int getID() {
@@ -21,6 +30,10 @@ public class Tile {
     }
 
     public void render(Graphics graphics, int x, int y){
-        graphics.drawImage(texture, x, y, TILE_WIDTH, TILE_HEIGHT,null);
+        graphics.drawImage(texture, x, y, TILE_WIDTH, TILE_HEIGHT, null);
+    }
+
+    public boolean isSolid(){
+        return false;
     }
 }

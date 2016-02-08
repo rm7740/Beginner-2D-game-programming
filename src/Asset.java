9bdeaa42d6aec3b49b965bce3720c8d107b422ac
rm7740 <adavid8a@hotmail.com>
2016-02-08 13:59:54 -0500

    Add an init method on Asset that loads all sprites

--- src/Asset.java
+++ src/Asset.java
@@ -1,5 +1,20 @@
-/**
- * Created by Dell on 8/2/2016.
- */
+import java.awt.image.BufferedImage;
+
 public class Asset {
+
+    public static BufferedImage player, pokemon, town;
+
+    private static final int playerWidth = 14, playerHeight = 16;
+    private static final int pokemonWidth = 56, pokemonHeight = 56;
+    private static final int townWidth = 320, townHeight = 288;
+
+    public static void init(){
+        SpriteSheet playerSheet = new SpriteSheet(ImageLoader.loadImage("/textures/Game Boy GBC - Pokemon Gold Silver - Gold.gif"));
+        SpriteSheet pokemonSheet = new SpriteSheet(ImageLoader.loadImage("/textures/Game Boy GBC - Pokemon Gold Silver - Pokemon Silver Version.png"));
+        SpriteSheet townSheet = new SpriteSheet(ImageLoader.loadImage("/textures/Game Boy GBC - Pokemon Gold Silver - Pallet Town.png"));
+
+        player = playerSheet.crop(58, 4, playerWidth, playerHeight);
+        pokemon = pokemonSheet.crop(72, 152, pokemonWidth, pokemonHeight);
+        town = townSheet.crop(6, 5, townWidth, townHeight);
+    }
 }

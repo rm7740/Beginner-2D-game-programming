305d1d99c1642a3773acd70e22146ef69d626042
rm7740 <adavid8a@hotmail.com>
2016-02-08 11:15:43 -0500

    Create the constructor & createDisplay method on Display

--- src/Display.java
+++ src/Display.java
@@ -1,5 +1,24 @@
-/**
- * Created by Dell on 8/2/2016.
- */
+import javax.swing.*;
+
 public class Display {
+
+    private JFrame frame;
+    private String title;
+    private int height, width;
+
+    public Display(String title, int width, int height) {
+        this.title = title;
+        this.width = width;
+        this.height = height;
+        createDisplay();
+    }
+
+    private void createDisplay() {
+        frame = new JFrame(title);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(width, height);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
 }

2e4faf2cb75158b9dab32a57cd5854e7c898bca3
rm7740 <adavid8a@hotmail.com>
2016-02-08 15:43:07 -0500

    Add constructor, update & render abstract methods on Entity

--- src/Entity.java
+++ src/Entity.java
@@ -1,5 +1,14 @@
-/**
- * Created by Dell on 8/2/2016.
- */
-public class Entity {
+import java.awt.*;
+
+public abstract class Entity {
+
+    protected float x, y;
+
+    public Entity(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public abstract update();
+    public abstract render(Graphics graphics);
 }

63b105e86c2128f1694ee5d7567ad6d75ff0a62b
rm7740 <adavid8a@hotmail.com>
2016-02-08 12:56:20 -0500

    Add the constructor and crop method on SpriteSheet

--- src/SpriteSheet.java
+++ src/SpriteSheet.java
@@ -1,5 +1,14 @@
-/**
- * Created by Dell on 8/2/2016.
- */
+import java.awt.image.BufferedImage;
+
 public class SpriteSheet {
+
+    private BufferedImage sheet;
+
+    public SpriteSheet(BufferedImage sheet) {
+        this.sheet = sheet;
+    }
+
+    public BufferedImage crop(int x, int y, int width, int height){
+        return sheet.getSubimage(x, y, width, height);
+    }
 }

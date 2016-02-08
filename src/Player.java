c3982f5b1c1cf4c371f38a41cba47a69ce69fd4a
rm7740 <adavid8a@hotmail.com>
2016-02-08 16:02:43 -0500

    Implement the abstract methods on Player

--- src/Player.java
+++ src/Player.java
@@ -1,5 +1,18 @@
-/**
- * Created by Dell on 8/2/2016.
- */
-public class Player {
+import java.awt.*;
+
+public class Player extends LiveEntity{
+
+    public Player(float x, float y) {
+        super(x, y);
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void render(Graphics graphics) {
+        graphics.drawImage(Asset.player, (int) x, (int) y, null);
+    }
 }

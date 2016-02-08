68cb0327db4d8efc999b4e51a6755061c0790950
rm7740 <adavid8a@hotmail.com>
2016-02-08 18:28:12 -0500

    Add more assets on Asset

--- src/Asset.java
+++ src/Asset.java
@@ -3,12 +3,23 @@ import java.awt.image.BufferedImage;
 public class Asset {
 
     public static BufferedImage player;
+    public static BufferedImage grass;
+    public static BufferedImage rock;
+    public static BufferedImage dirt;
 
-    private static final int playerWidth = 64, playerHeight = 64;
+    private static final int playerSide = 64;
+    private static final int grassSide = 32;
+    private static final int rockSide = 32;
+    private static final int dirtSide = 32;
     //Loads the necessary sprite sheets and crops them
     public static void init(){
         SpriteSheet playerSheet = new SpriteSheet(ImageLoader.loadImage("/textures/simple_clothes.gif"));
-
-        player = playerSheet.crop(0, playerHeight*2, playerWidth, playerHeight);
+        SpriteSheet grassSheet = new SpriteSheet(ImageLoader.loadImage("/textures/grass.png"));
+        SpriteSheet rockSheet = new SpriteSheet(ImageLoader.loadImage("/textures/rock.png"));
+        SpriteSheet dirtSheet = new SpriteSheet(ImageLoader.loadImage("/textures/dirt.png"));
+        player = playerSheet.crop(0, playerSide * 2, playerSide, playerSide);
+        grass = grassSheet.crop(0, grassSide * 5, grassSide, grassSide);
+        rock = rockSheet.crop(0, 0, rockSide, rockSide);
+        dirt = dirtSheet.crop(0, dirtSide* 5, dirtSide, dirtSide);
     }
 }

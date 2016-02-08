754dfe9a5bd0c78bf9b9064503a504a2e9f70f44
rm7740 <adavid8a@hotmail.com>
2016-02-08 16:49:41 -0500

    Write update on Player & fix the instanciation in GameState

--- src/Player.java
+++ src/Player.java
@@ -2,13 +2,27 @@ import java.awt.*;
 
 public class Player extends LiveEntity{
 
-    public Player(float x, float y) {
+    private Game game;
+
+    public Player(Game game, float x, float y) {
         super(x, y);
+        this.game = game;
     }
 
     @Override
     public void update() {
-
+        if(game.getKeyManager().up){
+            y -= 3;
+        }
+        if(game.getKeyManager().down){
+            y += 3;
+        }
+        if(game.getKeyManager().left){
+            x -= 3;
+        }
+        if(game.getKeyManager().right){
+            x += 3;
+        }
     }
 
     @Override

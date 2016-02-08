1de69b6d6a3436f3b83d9edf9b02f6dd412890ab
rm7740 <adavid8a@hotmail.com>
2016-02-08 11:22:36 -0500

    Add a canvas to the frame on createDisplay on Display

--- src/Display.java
+++ src/Display.java
@@ -1,8 +1,10 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Display {
 
     private JFrame frame;
+    private Canvas canvas;
     private String title;
     private int height, width;
 
@@ -20,5 +22,13 @@ public class Display {
         frame.setResizable(false);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+
+        canvas = new Canvas();
+        canvas.setPreferredSize(new Dimension(width, height));
+        canvas.setMaximumSize(new Dimension(width, height));
+        canvas.setMinimumSize(new Dimension(width, height));
+
+        frame.add(canvas);
+        frame.pack();
     }
 }

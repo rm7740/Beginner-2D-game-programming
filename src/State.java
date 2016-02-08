8e2d97fe117782e9fbf40ed222d1921ced107940
rm7740 <adavid8a@hotmail.com>
2016-02-08 15:38:33 -0500

    Add update & render abstract methods, getter and setter

--- src/State.java
+++ src/State.java
@@ -1,5 +1,17 @@
-/**
- * Created by Dell on 8/2/2016.
- */
-public class State {
+import java.awt.*;
+
+public abstract class State {
+
+    private static State currentState = null;
+
+    public static void setCurrentState(State currentState) {
+        State.currentState = currentState;
+    }
+
+    public static State getCurrentState() {
+        return currentState;
+    }
+
+    public abstract void update();
+    public abstract void render(Graphics graphics);
 }

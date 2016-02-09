385f735efe5419734390e348006cb9cac6016336
rm7740 <adavid8a@hotmail.com>
2016-02-09 13:44:49 -0500

    Add implemented methods

--- src/Tree.java
+++ src/Tree.java
@@ -1,5 +1,18 @@
-/**
- * Created by Dell on 9/2/2016.
- */
-public class Tree {
+import java.awt.*;
+
+public class Tree extends StaticEntity{
+
+    public Tree(Handler handler, float x, float y, int width, int height) {
+        super(handler, x, y, Tile.TILE_WIDTH * 3, Tile.TILE_HEIGHT * 4);
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void render(Graphics graphics) {
+        graphics.drawImage(Asset.greenTree, (int) (x - handler.getGameCamera().getxOffset()), (int) (y - handler.getGameCamera().getyOffset()), width, height, null);
+    }
 }

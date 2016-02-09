b0278ec61c34c13bea6f100d05d7ea2b301e1d19
rm7740 <adavid8a@hotmail.com>
2016-02-08 20:23:27 -0500

    Add method to move the camera on GameCamera

--- src/GameCamera.java
+++ src/GameCamera.java
@@ -1,5 +1,30 @@
-/**
- * Created by Dell on 8/2/2016.
- */
+
 public class GameCamera {
+
+    private float xOffset, yOffset;
+
+    public GameCamera(float xOffset, float yOffset) {
+        this.xOffset = xOffset;
+        this.yOffset = yOffset;
+    }
+
+    public void move(float x, float y){
+        xOffset += x;
+        yOffset += y;
+    }
+    public float getxOffset() {
+        return xOffset;
+    }
+
+    public void setxOffset(float xOffset) {
+        this.xOffset = xOffset;
+    }
+
+    public float getyOffset() {
+        return yOffset;
+    }
+
+    public void setyOffset(float yOffset) {
+        this.yOffset = yOffset;
+    }
 }

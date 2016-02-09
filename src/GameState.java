3c84ac056921be4df4a00ef822e41985108f6938
rm7740 <adavid8a@hotmail.com>
2016-02-09 13:52:17 -0500

    Add EntityManager class

--- src/GameState.java
+++ src/GameState.java
@@ -2,28 +2,21 @@ import java.awt.*;
 
 public class GameState extends State{
 
-    private Player player;
     private World world;
-    private Tree tree;
 
     public GameState(Handler handler) {
         super(handler);
         world = new World(handler, "res/worlds/world1.txt");
         handler.setWorld(world);
-        player = new Player(handler, 100, 100);
-        tree = new Tree(handler, 150, 150);
     }
 
     @Override
     public void update() {
         world.update();
-        player.update();
     }
 
     @Override
     public void render(Graphics graphics) {
         world.render(graphics);
-        tree.render(graphics);
-        player.render(graphics);
     }
 }

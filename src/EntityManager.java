1cbda315e3ad6c328b8fb0e1e14e0e506a40e9e4
rm7740 <adavid8a@hotmail.com>
2016-02-09 14:28:57 -0500

    Add update, render & addEntity on EntityManager

--- src/EntityManager.java
+++ src/EntityManager.java
@@ -1,3 +1,58 @@
+import java.awt.*;
+import java.util.ArrayList;
 
 public class EntityManager {
+
+    private Handler handler;
+    private Player player;
+    private ArrayList<Entity> entities;
+
+    public EntityManager(Handler handler, Player player) {
+        this.handler = handler;
+        this.player = player;
+        entities = new ArrayList<Entity>();
+    }
+
+    public void update(){
+        for(int i = 0; i < entities.size(); i++){
+            Entity entity = entities.get(1);
+            entity.update();
+        }
+        player.update();
+    }
+
+    public void render(Graphics graphics){
+        for(Entity entity: entities){
+            entity.render(graphics);
+        }
+        player.render(graphics);
+    }
+
+    public void addEntity(Entity entity){
+        entities.add(entity);
+    }
+
+    public Handler getHandler() {
+        return handler;
+    }
+
+    public void setHandler(Handler handler) {
+        this.handler = handler;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public ArrayList<Entity> getEntities() {
+        return entities;
+    }
+
+    public void setEntities(ArrayList<Entity> entities) {
+        this.entities = entities;
+    }
 }

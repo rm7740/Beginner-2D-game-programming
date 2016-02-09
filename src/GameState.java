7a999cb7b7ad234f23467b45bae4207cf554ac2b
rm7740 <adavid8a@hotmail.com>
2016-02-08 19:32:53 -0500

    Instanciate a World in the GameState

--- src/GameState.java
+++ src/GameState.java
@@ -3,10 +3,12 @@ import java.awt.*;
 public class GameState extends State{
 
     private Player player;
+    private World world;
 
     public GameState(Game game) {
         super(game);
         player = new Player(game, 100, 100);
+        world = new World("res/worlds/world1.txt");
     }
 
     @Override
@@ -16,6 +18,7 @@ public class GameState extends State{
 
     @Override
     public void render(Graphics graphics) {
+        world.render(graphics);
         player.render(graphics);
     }
 }

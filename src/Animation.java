0e59c4672188db80a705cdeddfcaa26690ff6e88
rm7740 <adavid8a@hotmail.com>
2016-02-08 23:28:24 -0500

    Add constructor, update & getCurrentAnimationFrame methods on Animation

--- src/Animation.java
+++ src/Animation.java
@@ -1,5 +1,33 @@
-/**
- * Created by Dell on 8/2/2016.
- */
+import java.awt.image.BufferedImage;
+
 public class Animation {
+
+    private int speed, index;
+    private BufferedImage[] animationFrames;
+    private long lastTime, timer;
+
+    public Animation(int speed, BufferedImage[] animationFrames) {
+        this.speed = speed;
+        this.animationFrames = animationFrames;
+        index = 0;
+        timer = 0;
+        lastTime = System.currentTimeMillis();
+    }
+
+    public BufferedImage getCurrentAnimationFrame(){
+        return animationFrames[index];
+    }
+
+    public void update(){
+        timer += System.currentTimeMillis() - lastTime;
+        lastTime = System.currentTimeMillis();
+
+        if(timer >= speed){
+            index++;
+            timer = 0;
+            if(index >= animationFrames.length){
+                index = 0;
+            }
+        }
+    }
 }

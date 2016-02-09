90fc51639d7bae34b331e0462aecb21a9be8b42a
rm7740 <adavid8a@hotmail.com>
2016-02-09 14:42:00 -0500

    Add bounds to Tree

--- src/Tree.java
+++ src/Tree.java
@@ -4,6 +4,11 @@ public class Tree extends StaticEntity{
 
     public Tree(Handler handler, float x, float y) {
         super(handler, x, y, Tile.TILE_WIDTH * 3, Tile.TILE_HEIGHT * 4);
+
+        bounds.x = 32;
+        bounds.y = 80;
+        bounds.width = 20;
+        bounds.height = 28;
     }
 
     @Override
@@ -13,6 +18,10 @@ public class Tree extends StaticEntity{
 
     @Override
     public void render(Graphics graphics) {
-        graphics.drawImage(Asset.greenTree, (int) (x - handler.getGameCamera().getxOffset()), (int) (y - handler.getGameCamera().getyOffset()), width, height, null);
+        graphics.drawImage(Asset.greenTree, (int) (x - handler.getGameCamera().getxOffset()),
+                (int) (y - handler.getGameCamera().getyOffset()), width, height, null);
+        //graphics.setColor(Color.BLUE);
+        //graphics.fillRect((int) (x + bounds.x - handler.getGameCamera().getxOffset()),
+        //        (int) (y + bounds.y - handler.getGameCamera().getyOffset()), bounds.width, bounds.height);
     }
 }

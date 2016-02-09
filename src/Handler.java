a71defd2f3902e2383e982f3abe6c009191fa305
rm7740 <adavid8a@hotmail.com>
2016-02-08 21:07:42 -0500

    Add getters for objects accessed through Game & World

--- src/Handler.java
+++ src/Handler.java
@@ -1,5 +1,42 @@
-/**
- * Created by Dell on 8/2/2016.
- */
+
 public class Handler {
+
+    private Game game;
+    private World world;
+
+    public Handler(Game game) {
+        this.game = game;
+    }
+
+    public int getWidth(){
+        return game.getWidth();
+    }
+
+    public int getHeight(){
+        return game.getHeight();
+    }
+
+    public KeyManager getKeyManager(){
+        return game.getKeyManager();
+    }
+
+    public GameCamera getGameCamera(){
+        return game.getGameCamera();
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public World getWorld() {
+        return world;
+    }
+
+    public void setWorld(World world) {
+        this.world = world;
+    }
 }

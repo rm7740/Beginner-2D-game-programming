7ae210bb8817328ddaf4cf51593a9048977f3268
rm7740 <adavid8a@hotmail.com>
2016-02-08 22:50:04 -0500

    Add checkBlankSpace method on GameCamera to show only worldTiles

--- src/GameCamera.java
+++ src/GameCamera.java
@@ -10,14 +10,30 @@ public class GameCamera {
         this.yOffset = yOffset;
     }
 
+    public void checkBlankSpace(){
+        if(xOffset < 0){
+            xOffset = 0;
+        } else if(xOffset > handler.getWorld().getWidth() * Tile.TILE_WIDTH - handler.getWidth()){
+            xOffset = handler.getWorld().getWidth() * Tile.TILE_WIDTH - handler.getWidth();
+        }
+
+        if(yOffset < 0){
+            yOffset = 0;
+        } else if(yOffset > handler.getWorld().getHeight() * Tile.TILE_HEIGHT - handler.getHeight()){
+            yOffset = handler.getWorld().getHeight() * Tile.TILE_HEIGHT - handler.getHeight();
+        }
+    }
+
     public void centerOnEntity(Entity entity){
         xOffset = entity.getX() - handler.getWidth() / 2 + entity.getWidth() / 2;
         yOffset = entity.getY() - handler.getHeight() / 2 + entity.getHeight() / 2;
+        checkBlankSpace();
     }
 
     public void move(float x, float y){
         xOffset += x;
         yOffset += y;
+        checkBlankSpace();
     }
     public float getxOffset() {
         return xOffset;

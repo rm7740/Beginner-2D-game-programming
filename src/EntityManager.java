0dec04fcbff223a906717bf745b0ba7bbe580c27
rm7740 <adavid8a@hotmail.com>
2016-02-09 15:10:48 -0500

    Add a Comparator on EntityManager to render in the correct order

--- src/EntityManager.java
+++ src/EntityManager.java
@@ -1,11 +1,21 @@
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class EntityManager {
 
     private Handler handler;
     private Player player;
     private ArrayList<Entity> entities;
+    private Comparator<Entity> renderSort = new Comparator<Entity>() {
+        @Override
+        public int compare(Entity a, Entity b) {
+            if(a.getY() + a.getHeight() < b.getY() + b.getHeight()){
+                return -1;
+            }
+            return 1;
+        }
+    };
 
     public EntityManager(Handler handler, Player player) {
         this.handler = handler;
@@ -19,6 +29,7 @@ public class EntityManager {
             Entity entity = entities.get(i);
             entity.update();
         }
+        entities.sort(renderSort);
     }
 
     public void render(Graphics graphics){

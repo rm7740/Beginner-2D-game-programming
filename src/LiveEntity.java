ba0cb569982382ba116d80ed867b31278bfdbe76
rm7740 <adavid8a@hotmail.com>
2016-02-09 14:59:40 -0500

    Fix move on LiveEntity to use collision detection

--- src/LiveEntity.java
+++ src/LiveEntity.java
@@ -18,8 +18,12 @@ public abstract class LiveEntity extends Entity{
     }
 
     public void move(){
-        moveX();
-        moveY();
+        if (!checkEntityCollisions(xMove, 0f)) {
+            moveX();
+        }
+        if(!checkEntityCollisions(0f, yMove)) {
+            moveY();
+        }
     }
 
     public void moveX(){
